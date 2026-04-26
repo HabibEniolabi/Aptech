c6890cb91de80319d9d679754d159d1dce97d0bd
HabibEniolabi <habibullahayomide009@gmail.com>
2026-04-27 00:39:30 +0100

    modify: homepage hero section

--- src/pages/UberPage.jsx
+++ src/pages/UberPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 const WizkidFanPage = () => {
   return (
     <main className="w-full">
       <Navbar />
+      <div className="px-10 py-12">
+        <HeroSection />
+      </div>
     </main>
   );
 };
